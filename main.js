c975b0e6068cbb42c5f9f1b7567e5cab31ebc5f9
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-02-22 14:51:18 -0600

    changes

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
-
+function fizzy(num) {
+  if (num / 3) {
+    //return 'Fizz';
+  } else if (num / 5) {
+    return 'Buzz';
+  } else if (num / 3 && num / 5) {
+      return 'Buzz';
+    } else (num); {
+    return num;
+  }
+}
 
 
 
