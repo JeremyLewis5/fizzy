28cdc32c64cdca8a75e7368441ad681d3f08a0ee
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-02-22 21:07:31 -0600

    change 2

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 function fizzy(num) {
-  if (num / 3) {
-    //return 'Fizz';
-  } else if (num / 5) {
+   if (num % 3 === 0) {
+    return 'Fizz';
+  } else if (num % 5 === 0) {
     return 'Buzz';
-  } else if (num / 3 && num / 5) {
-      return 'Buzz';
-    } else (num); {
+  } else if (num % 3 === 0 && num % 5 === 0) {
+      return 'FizzBuzz';
+    } else  {
     return num;
   }
 }
